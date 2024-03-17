9685dfa3ee30e64a183b6821057042ad093f9d77
Kiyotaka327 <andreimandrique3@gmail.com>
2024-03-17 16:11:56 +0800

    finish the calculator .
    I will refactor my code to this project in the future

--- index.js
+++ index.js
@@ -23,13 +23,18 @@ function operate(operator, firstNum, secondNum) {
 
 const display = document.getElementById("display");
 const btn = document.querySelectorAll("#btn");
-
-let haveFirstNum = false;
-let haveOperator = false;
-let haveSecondNum = false;
+const step = document.getElementById("step");
 
 let firstNumberContainer;
 let secondNumberContainer;
+let operatorContainer;
+
+let haveFirstNumber;
+let haveSecondNumber;
+
+let haveOutput = false;
+
+let haveOperator = false;
 
 btn.forEach((button) => {
   button.classList.add("btn-design");
@@ -37,28 +42,70 @@ btn.forEach((button) => {
   button.addEventListener("click", () => {
 
     buttonContent = button.textContent;
-    const buttonNumber = Number(buttonContent);
 
     if((buttonContent == '+')||(buttonContent == '-')||
-        (buttonContent == 'X')||(buttonContent == '/')){
-          console.log(buttonContent);
+      (buttonContent == 'X')||(buttonContent == '/')){
+
+      if(haveFirstNumber){
+        
+        if(haveFirstNumber && haveSecondNumber){
+          console.log("Invalid you can only press = or Clear");
+        }else{
+          operatorContainer = buttonContent;
+          console.log(`Your operator ${operatorContainer}`);
+          step.textContent = `Operator ${operatorContainer}`;
           display.textContent = '';
           haveOperator = true;
+        }
+        
+      }else{
+        console.log("no operator");
+      }
     }
-    else if(haveFirstNum == false){
-      display.append(buttonContent);
-      firstNumberContainer = display.textContent;
-      console.log(firstNumberContainer);
-      haveFirstNum = true;
+    else if((buttonContent == '=')||(buttonContent == 'C')){
+      if(buttonContent == 'C'){
+        display.textContent = '';
+        haveOperator = false;
+        haveFirstNumber = false;
+        haveSecondNumber = false;
+        step.textContent = "cleared";
+      }
+      if(buttonContent == '='){
+        const firstNum = Number(firstNumberContainer);
+        const secondNum = Number(secondNumberContainer);
+        const output = operate(operatorContainer, firstNum, secondNum);
+        step.textContent = "Output";
+        console.log(output);
+        display.textContent = output;
+        haveOutput = true;
+      }
     }
-    else if(haveFirstNum== true){
-      display.append(buttonContent);
-      secondNumberContainer = display.textContent;
-      console.log(secondNumberContainer);
-      haveSecondNum = true;
-    }
+    else{
+      
+      if(haveOutput){
+        haveOperator = false;
+        haveFirstNumber = false;
+        haveSecondNumber = false;
+        display.textContent = '';
+      }
 
-
+      if(haveOperator){
+        display.append(buttonContent);
+        secondNumberContainer = display.textContent;
+        console.log(`SecondNumber: ${secondNumberContainer}`);
+        step.textContent = "secondNumber";
+        haveSecondNumber = true;
+        haveOutput = false;
+      }
+      else{
+        display.append(buttonContent);
+        firstNumberContainer = display.textContent;
+        console.log(`FirstNumber: ${firstNumberContainer}`);
+        step.textContent = "firstNumber";
+        haveFirstNumber = true;
+        haveOutput = false;
+      }
+    }
 
   });
 });
